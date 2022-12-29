65149a9f7ffb2cba8d14dcb1322df75de44980aa
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2022-12-29 21:49:24 +0300

    Add files via upload
    Copyright year changes automatically.
    JS code is now all in one place.

--- static/script.js
+++ static/script.js
@@ -42,3 +42,42 @@ document.addEventListener('DOMContentLoaded', function () {
     loader = document.getElementById('loader');
     loadNow(1);
 });
+
+/* ----------------------------------------------------------- */
+/*  SCROLL TO TOP BUTTON
+  /* ----------------------------------------------------------- */
+//Get the button
+let mybutton = document.getElementById('btn-back-to-top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = 'block';
+    } else {
+        mybutton.style.display = 'none';
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop);
+
+function backToTop() {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: 'smooth',
+    });
+}
+
+/* ----------------------------------------------------------- */
+/*  COPYRIGHT YEAR
+  /* ----------------------------------------------------------- */
+
+document.querySelector('.copyright-year').textContent =
+    new Date().getFullYear();
